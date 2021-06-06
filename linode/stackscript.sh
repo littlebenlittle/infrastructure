8afa3aba453f20c3045c96b489e71f31b7644142
Ben Little <ben.little@benlittle.dev>
2021-06-05 22:19:41 -0400

    add stackscript

---
+++ linode/stackscript.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+dnf -y upgrade
+dnf -y install mosh iptables-services
+systemctl disable firewalld
+systemctl enable iptables
+systemctl enable ip6tables
+
+dnf remove firewalld
+
+# ip4
+# iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+# iptables -A INPUT -i lo -j ACCEPT
+# iptables -A INPUT -p icmp -j ACCEPT
+iptables -A INPUT -p tcp -m multiport -m state --destination-ports 22,53,80,443   --state NEW -j ACCEPT
+iptables -A INPUT -p udp -m multiport -m state --destination-ports 53,60000:60100 --state NEW -j ACCEPT
+iptables -A INPUT -m limit --limit 5/min -j LOG --log-prefix "iptables_INPUT_denied: " --log-level 7
+iptables -A INPUT -j REJECT
+iptables -A FORWARD -m limit --limit 5/min -j LOG --log-prefix "iptables_FORWARD_denied: " --log-level 7
+iptables -A FORWARD  -j REJECT
+iptables COMMIT
+
+# ip6
+ip6tables -P INPUT DROP
+ip6tables -P FORWARD DROP
+ip6tables -P OUTPUT DROP
+ip6tables COMMIT
+
+hostnamectl set-hostname "$LINODE_ID-$LINODE_DATACENTERID"
+useradd -m user
+usermod -aG wheel user
+
+cat >/root/setup.sh <<EOF
+#!/bin/bash
+
+passwd user
+
+sed -i \
+    -e 's/PasswordAuthentication yes/PasswordAuthentication no/g' \
+    -e 's/PermitRootLogin yes/PermitRootLogin no/g' \
+    -e 's/#AddressFamily any/AddressFamily inet/g' \
+    /etc/ssh/sshd_config
+
+systemctl restart sshd
+EOF
+chmod +x /root/setup.sh
